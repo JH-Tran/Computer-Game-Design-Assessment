9777dce8715d546eb7da183994ab960d1590df71
Justin Tran <jhtran023@gmail.com>
2023-03-30 13:48:12 +1100

    Cursor lock and not visible

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
+        }
+    }
+}
