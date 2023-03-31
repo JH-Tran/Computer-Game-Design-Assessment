791f221fa41654d4de1edd4f8e2e17d96cd8aa5e
Kyaw Thiha <alexplayer153@gmail.com>
2023-03-31 16:06:04 +1100

    Battery meter design updated

---
+++ Assets/Scripts/Donre_Battery_v2.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Donre_Battery_v2 : MonoBehaviour
+{
+    [SerializeField] DroneMovement droneMovement;
+    [SerializeField] DroneCamera droneCamera;
+    [SerializeField] private Image batteryInner;
+
+    private float batteryMaxTime = 15f;
+    [SerializeField] private float batteryTime;
+    private float batteryCooldownTime = 5f;
+
+    private bool isTimerOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerReset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        if (isTimerOn)
+        {
+            if (batteryTime > 0)
+            {
+                batteryTime -= Time.deltaTime;
+                batteryInner.fillAmount = batteryTime/batteryMaxTime;
+            }
+            else
+            {
+                batteryTime = 0;
+                isTimerOn = false;
+                droneCamera.changeDroneState(isTimerOn);
+                droneMovement.changeDroneState(isTimerOn);
+                StartCoroutine(activeDrone(batteryCooldownTime));
+            }
+        }
+    }
+
+    public void TimerReset()
+    {
+        //batteryFillIndicator.transform.localScale = new Vector3(.1f, batteryFillIndicator.transform.localScale.y, batteryFillIndicator.transform.localScale.z);
+        batteryInner.fillAmount = 1;
+        batteryTime = batteryMaxTime;
+        isTimerOn = true;
+        droneCamera.changeDroneState(isTimerOn);
+        droneMovement.changeDroneState(isTimerOn);
+    }
+    IEnumerator activeDrone(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        TimerReset();
+    }
+}
