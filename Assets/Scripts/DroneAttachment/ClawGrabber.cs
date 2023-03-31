ca836c561fd7ded27667aabd12ad088ce5d63f8b
Justin Tran <jhtran023@gmail.com>
2023-04-01 10:54:06 +1100

    Claw Dropping Item

--- Assets/Scripts/DroneAttachment/ClawGrabber.cs
+++ Assets/Scripts/DroneAttachment/ClawGrabber.cs
@@ -7,12 +7,28 @@ public class ClawGrabber : MonoBehaviour
     [SerializeField] private BoxCollider clawCollider;
     [SerializeField] private Transform clawInitalPosition;
 
-    private bool isObjectGrab;
-    private bool isObstacle;
+    public bool isGrabbingObject = false;
+    public bool isObjectHold = false;
+    public List<GameObject> objectGrabbed = new List<GameObject>();
 
     private void Start()
     {
 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            if (isGrabbingObject == false)
+            {
+                isGrabbingObject = true;
+            }
+            else if (isGrabbingObject == true)
+            {
+                DropObject();
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -20,13 +36,33 @@ public class ClawGrabber : MonoBehaviour
         Debug.Log(other.name);
         if (other.CompareTag("Object"))
         {
-            other.transform.parent = gameObject.transform;
-            Destroy(other.GetComponent<Rigidbody>());
+            if (isGrabbingObject == true && isObjectHold == false)
+            {
+                other.transform.parent = gameObject.transform;
+                objectGrabbed.Add(other.gameObject);
+                isObjectHold = true;
+                Destroy(other.GetComponent<Rigidbody>());
+            }
         }
     }
 
-    public void ClawMovement()
+    public void DropObject()
     {
-
+        if (objectGrabbed.Count > 0 && isObjectHold == true)
+        {
+            for (int i = 0; i < objectGrabbed.Count; i++)
+            {
+                if (objectGrabbed[i].GetComponent<Rigidbody>() == null)
+                {
+                    objectGrabbed[i].gameObject.AddComponent<Rigidbody>();
+                    Debug.Log("BODY");
+                }
+                objectGrabbed[i].transform.parent = null;
+                Debug.Log("PARENT");
+            }
+            objectGrabbed.Clear();
+            isObjectHold = false;
+        }
+        isGrabbingObject = false;
     }
 }
