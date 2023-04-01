a2396f29cfd3b9c1625e8543b69044462f1207f7
Justin Tran <jhtran023@gmail.com>
2023-04-01 12:21:40 +1100

    Removed Redundant Assets
    Removed the previous drone battery and tablet location. Rename drone battery version 2 to just drone battery.

--- Assets/Scripts/DroneBattery.cs
+++ Assets/Scripts/DroneBattery.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DroneBattery : MonoBehaviour
+public class DonreBattery : MonoBehaviour
 {
-    [SerializeField] GameObject batteryFillIndicator;
     [SerializeField] DroneMovement droneMovement;
     [SerializeField] DroneCamera droneCamera;
+    [SerializeField] private Image batteryInner;
 
-    //Timers are in seconds
-    private float batteryMaxTime = 10f;
-    private float batteryTime;
+    private float batteryMaxTime = 15f;
+    [SerializeField] private float batteryTime;
     private float batteryCooldownTime = 5f;
 
     private bool isTimerOn;
 
+    // Start is called before the first frame update
     void Start()
     {
         TimerReset();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     void FixedUpdate()
     {
         if (isTimerOn)
@@ -27,7 +33,7 @@ public class DroneBattery : MonoBehaviour
             if (batteryTime > 0)
             {
                 batteryTime -= Time.deltaTime;
-                batteryFillIndicator.transform.localScale = new Vector3 (batteryTime/batteryMaxTime, batteryFillIndicator.transform.localScale.y, batteryFillIndicator.transform.localScale.z);
+                batteryInner.fillAmount = batteryTime/batteryMaxTime;
             }
             else
             {
@@ -42,13 +48,13 @@ public class DroneBattery : MonoBehaviour
 
     public void TimerReset()
     {
-        batteryFillIndicator.transform.localScale = new Vector3(.1f, batteryFillIndicator.transform.localScale.y, batteryFillIndicator.transform.localScale.z);
+        //batteryFillIndicator.transform.localScale = new Vector3(.1f, batteryFillIndicator.transform.localScale.y, batteryFillIndicator.transform.localScale.z);
+        batteryInner.fillAmount = 1;
         batteryTime = batteryMaxTime;
         isTimerOn = true;
         droneCamera.changeDroneState(isTimerOn);
         droneMovement.changeDroneState(isTimerOn);
     }
-
     IEnumerator activeDrone(float delay)
     {
         yield return new WaitForSeconds(delay);
