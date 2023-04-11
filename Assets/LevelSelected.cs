931affd41745804195369dbde50373e19a636fb2
Thien Phu Tran <Thien.P.Tran-1@student.uts.edu.au>
2023-04-11 20:40:29 +1000

    level button done

---
+++ Assets/LevelSelected.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelected : MonoBehaviour
+{
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+}
+
