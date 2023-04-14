fd2e66f0ed4ec6fecf831e1ed3bb7a17d18aaf73
Justin Tran <jhtran023@gmail.com>
2023-04-14 22:14:39 +1000

    Player interaction change colour state
    Bug fixed where player colour state doesn't change.

--- Assets/Scripts/Buttons/TestButtonBlue.cs
+++ Assets/Scripts/Buttons/TestButtonBlue.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestButtonBlue : MonoBehaviour
+public class TestButtonBlue : MonoBehaviour, IInteractable
 {
     private GameObject playerObject;
     public void Start()
