7f23dafa3ebeb5e5b167c5e92048237a0f7bbe58
Justin Tran <jhtran023@gmail.com>
2023-04-18 22:06:29 +1000

    Camera Position Points Forward When Spawning

--- Assets/Scripts/Drone/DroneCamera.cs
+++ Assets/Scripts/Drone/DroneCamera.cs
@@ -16,7 +16,7 @@ public class DroneCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        droneCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
