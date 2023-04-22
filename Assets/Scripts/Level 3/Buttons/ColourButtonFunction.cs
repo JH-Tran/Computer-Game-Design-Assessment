6a7c3b90e13088c3b169a283d52b16e41fb9d80a
Justin Tran <jhtran023@gmail.com>
2023-04-22 14:54:31 +1000

    Introduction with Colour Change Feature

--- Assets/Scripts/Level 3/Buttons/ColourButtonFunction.cs
+++ Assets/Scripts/Level 3/Buttons/ColourButtonFunction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestButtonBlue : MonoBehaviour, IInteractable
+public class ColourButtonFunction : MonoBehaviour, IInteractable
 {
     [SerializeField] private Material materialType;
     [SerializeField] private List<GameObject> objectChanger = new List<GameObject>();
---
+++ Assets/Scripts/Level 3/Prototype/ColourObjectPlate.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourObjectPlate : MonoBehaviour
+{
+    //If the object has the same material as the door the door "opens".
+
+    [SerializeField] private GameObject Door;
+    [SerializeField] private Material keyMaterial;
+    private bool isOpen = false;
+
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        if (isOpen == true)
+        {
+            Door.SetActive(false);
+        }
+        else
+        {
+            Door.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Object"))
+        {
+            if (other.GetComponent<Renderer>().material =  keyMaterial)
+            {
+                isOpen = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Object"))
+        {
+            if (other.GetComponent<Renderer>().material = keyMaterial)
+            {
+                isOpen = false;
+            }
+        }
+    }
+}
