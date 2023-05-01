f8b7ddee94669e89aca66c1a3c135ff8f6808b13
Justin Tran <jhtran023@gmail.com>
2023-05-01 11:12:50 +1000

    Change pause button to escape
    From T due to testing without exiting the game view in Unity

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -23,7 +23,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (isGamePaused)
             {
